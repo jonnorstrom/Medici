45c280f6586f79a1e4faa0fdbee4ebd3afb4ca89
scottsouthard <scottmsouthard@gmail.com>
2016-09-16 11:34:07 -0500

    Tweak museum.website display

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -11,7 +11,7 @@ class Museum < ApplicationRecord
   has_attached_file :photo, styles: { large: "768x768>", medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
-  validates_presence_of :name, :blurb, :description, :photo, :price, :address
+  validates_presence_of :name, :blurb, :description, :photo, :price, :address, :opening_time, :closing_time, :website
   validates_uniqueness_of :name
 
   geocoded_by :address
