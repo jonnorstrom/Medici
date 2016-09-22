1b3969fbe74eb05bc9898cfb4c5639d751b1d220
scottsouthard <scottmsouthard@gmail.com>
2016-09-22 14:33:35 -0500

    Fix user tests, tweak email css

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,21 +7,21 @@ RSpec.describe User, :type => :model do
     subject.name = "Corbin Dallas"
     subject.password = "multipass"
     subject.email = "corbin@dallas.com"
-    expect(User.new).to be_valid
+    expect(subject).to be_valid
   end
   it "is not valid without an email" do
     subject.name = "Corbin Dallas"
     subject.password = "multipass"
-    expect(User.new).to_not be_valid
+    expect(subject).to_not be_valid
   end
   it "is not valid without a password"do
       subject.name = "Corbin Dallas"
       subject.email = "corbin@dallas.com"
-      expect(User.new).to_not be_valid
+      expect(subject).to_not be_valid
   end
   it "is not valid without a name" do
     subject.password = "multipass"
     subject.email = "corbin@dallas.com"
-    expect(User.new).to_not be_valid
+    expect(subject).to_not be_valid
   end
 end
