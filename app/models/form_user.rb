0188b08584dd741697bf7f424ae5014cdefd5564
scottsouthard <scottmsouthard@gmail.com>
2016-09-29 16:56:54 -0500

    Tweak Avatar for heroku

--- app/models/form_user.rb
+++ app/models/form_user.rb
@@ -8,8 +8,8 @@ class FormUser < User
   validates_uniqueness_of :email, allow_blank: true, if: :email_changed?
   validates_format_of     :email, with: Devise.email_regexp, allow_blank: true, if: :email_changed?
 
-  validates_presence_of :name
-  validates_presence_of :avatar
+  # validates_presence_of :name
+  # validates_presence_of :avatar
 
   validates_presence_of     :password, if: :password_required?
   validates_confirmation_of :password, if: :password_required?
