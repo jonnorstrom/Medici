f228465a99b0ee407aa75d7674cf6dcefa08f640
scottsouthard <scottmsouthard@gmail.com>
2016-10-12 12:18:09 -0500

    Adjust application erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,21 +21,4 @@ class User < ApplicationRecord
      @facebook_client ||= Facebook.client( access_token: facebook.accesstoken )
    end
 
-  #  def self.from_omniauth(auth)
-  #    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #      user.email = auth.info.email
-  #      user.password = Devise.friendly_token[0,20]
-  #      user.name = auth.info.name
-  #      user.avatar = auth.info.image
-  #    end
-  #  end
-  #
-  #  def self.new_with_session(params, session)
-  #   super.tap do |user|
-  #     if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-  #       user.email = data["email"] if user.email.blank?
-  #     end
-  #   end
-  # end
-
 end
