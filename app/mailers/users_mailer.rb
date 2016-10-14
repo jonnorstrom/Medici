2702da4fa25bf89be8b1b5367f476dc3ca60810c
scottsouthard <scottmsouthard@gmail.com>
2016-10-14 18:49:37 -0500

    Add signup mailer with contest terms

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -5,7 +5,7 @@ class UsersMailer < ApplicationMailer
   def signup_email(user)
     @user = user
     attachments.inline['logo.jpg'] = File.read("#{Rails.root}/app/assets/images/Medici_logos/Medici_logo_CMYK_sm.png")
-    mail(from: 'MediciMuseums <admin@medicimuseums.com>', to: @user.email, subject: 'Thanks for signing up for Medici Museums!')
+    mail(from: 'MediciMuseums <admin@medicimuseums.com>', to: @user.email, subject: 'Welcom to Medici!')
   end
 
 end
