0d927f1a09c047c4181328f51a6213e375b37e03
scottsouthard <scottmsouthard@gmail.com>
2016-10-14 15:33:42 -0500

    Adjust mailer for image

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -4,6 +4,7 @@ class UsersMailer < ApplicationMailer
 
   def signup_email(user)
     @user = user
+    attachments.inline['logo.jpg'] = File.read("#{Rails.root}/app/assets/images/Medici_logos/Medici_logo_CMYK_sm.png")
     mail(from: 'MediciMuseums <admin@medicimuseums.com>', to: @user.email, subject: 'Thanks for signing up for Medici Museums!')
   end
 
