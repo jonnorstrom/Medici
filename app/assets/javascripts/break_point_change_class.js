36829a050d81b40985e86d77b02d6e62a64832d0
PeterDrakeNichols <PeterDrakeNichols@gmail.com>
2016-10-14 14:10:43 -0500

    mobile refactor

--- app/assets/javascripts/break_point_change_class.js
+++ app/assets/javascripts/break_point_change_class.js
@@ -1,19 +1,20 @@
-$(window).load(function(){
-   var width = $(window).width();
-   if(width <= 768){
-       $('#dynamic-nav').removeClass('navbar-fixed-side').addClass('navbar-fixed-top');
-   }
-   else{
-       $('#dynamic-nav').removeClass('navbar-fixed-top').addClass('navbar-fixed-side');
-   }
-})
-
-$(window).resize(function(){
-   var width = $(window).width();
-   if(width <= 768){
-       $('#dynamic-nav').removeClass('navbar-fixed-side').addClass('navbar-fixed-top');
-   }
-   else{
-       $('#dynamic-nav').removeClass('navbar-fixed-top').addClass('navbar-fixed-side');
-   }
-})
+$(document).on('turbolinks:load', function(){
+    
+     var width = $(window).width();
+     if(width <= 768){
+          console.log("meow");
+         $('#dynamic-nav').removeClass('navbar-fixed-side').addClass('navbar-fixed-top');
+     }
+     else{
+         $('#dynamic-nav').removeClass('navbar-fixed-top').addClass('navbar-fixed-side');
+     }
+  $(window).resize(function(){
+     var width = $(window).width();
+     if(width <= 768){
+         $('#dynamic-nav').removeClass('navbar-fixed-side').addClass('navbar-fixed-top');
+     }
+     else{
+         $('#dynamic-nav').removeClass('navbar-fixed-top').addClass('navbar-fixed-side');
+     }
+  });
+});
