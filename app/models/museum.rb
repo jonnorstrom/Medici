ab84d957b094937b0692327e72cd06397d8277ae
scottsouthard <scottmsouthard@gmail.com>
2016-10-19 15:13:11 -0500

    Add validation for photo size

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -14,6 +14,7 @@ class Museum < ApplicationRecord
   validates_presence_of :name, :blurb, :description, :photo, :price, :address, :opening_time, :closing_time, :website
   validates_uniqueness_of :name
   validate :close_must_be_after_open
+  validate :image_dimensions
 
   geocoded_by :address
   after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
@@ -22,11 +23,24 @@ class Museum < ApplicationRecord
     "#{id} #{name}".parameterize
   end
 
+   private
+
+  def image_dimensions
+    if photo.queued_for_write[:original] != nil
+      required_width  = 400
+      required_height = 400
+      dimensions = Paperclip::Geometry.from_file(photo.queued_for_write[:original].path)
+
+      errors.add(:photo, "Width must be 400px") unless dimensions.width == required_width
+      errors.add(:photo, "Height must be 400px") unless dimensions.height == required_height
+    end
+  end
+
   def close_must_be_after_open
     if opening_time == nil || closing_time == nil
-      errors.add(:closing_time, "must be after start date")
+      errors.add(:closing_time, "must be after opening_time")
     elsif opening_time >= closing_time
-      errors.add(:closing_time, "must be after start date")
+      errors.add(:closing_time, "must be after opening_time")
     end
   end
 
