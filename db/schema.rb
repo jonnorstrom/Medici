a2aaefbd528bc7ba62c7a5f186573011699a1427
Jonathan Norstrom <jon.norstrom@gmail.com>
2016-11-30 17:38:28 -0600

    remove full_name from users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130191005) do
+ActiveRecord::Schema.define(version: 20161130233728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -190,7 +190,6 @@ ActiveRecord::Schema.define(version: 20161130191005) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
-    t.string   "full_name"
     t.string   "encrypted_password",     default: "",    null: false
     t.boolean  "admin",                  default: false
     t.string   "image_url"
