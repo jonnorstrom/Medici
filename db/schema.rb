d61a2b70c2bbb40c3263c2d2aa8a03c11d2143af
Jonathan Norstrom <jon.norstrom@gmail.com>
2016-12-01 10:40:58 -0600

    add full name to users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130233728) do
+ActiveRecord::Schema.define(version: 20161201163906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -211,6 +211,7 @@ ActiveRecord::Schema.define(version: 20161130233728) do
     t.datetime "avatar_updated_at"
     t.string   "first_name",             default: "",    null: false
     t.string   "last_name",              default: "",    null: false
+    t.string   "full_name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
