2194c12a268a5fd627f11b28455710e639369b11
Jonathan Norstrom <jon.norstrom@gmail.com>
2016-12-01 11:31:18 -0600

    p the auth

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -4,6 +4,8 @@ class Identity < ActiveRecord::Base
   validates_uniqueness_of :uid, :scope => :provider
 
   def self.find_for_oauth(auth)
+    p auth
+    p "here's the auth!"
     identity = find_by(provider: auth.provider, uid: auth.uid)
     identity = create(uid: auth.uid, provider: auth.provider) if identity.nil?
     identity.accesstoken = auth.credentials.token
