3c21b15e44b3906f81d05deec21c0f5047349bdd
PeterDrakeNichols <PeterDrakeNichols@gmail.com>
2016-12-07 16:00:24 -0600

    pop-up register

---
+++ app/assets/javascripts/register_pop_up.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+	if ($(window).width() <= 768){
+		var visited = localStorage.getItem('visited');
+		if (!visited) {
+	  		localStorage.setItem('visited', true);
+			$('#signUpModal').modal('show');
+		}	
+	}	
+
+});
