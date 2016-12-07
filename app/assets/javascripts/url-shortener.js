4e8d4f24d455e820ec78ee69a3dec289384cb6d1
PeterDrakeNichols <PeterDrakeNichols@gmail.com>
2016-12-07 10:38:56 -0600

    card clean-up

--- app/assets/javascripts/url-shortener.js
+++ app/assets/javascripts/url-shortener.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
 	var shortenURL = function(s){
 		sEnd = s.slice(s.length - 15);
-		sStart = s.replace('http://', '').replace('https://', '').substr(0,14);
-		var shorten = sStart + '...';
+		sStart = s.replace('http://www.', '').replace('https://www.', '').substr(0,14);
+		var shorten = sStart + '.com';
 		document.getElementById("venue-website").innerHTML = shorten;
 		return shorten;
 	}
-	shortenURL(document.getElementById("venue-website").innerHTML);
+	if(document.getElementById("venue-website")){
+		shortenURL(document.getElementById("venue-website").innerHTML);
+	} 
 });
