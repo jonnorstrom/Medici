a6b7b2eeb72f0ed20766b86d0e24b1fbfef50121
Jonathan Norstrom <jon.norstrom@gmail.com>
2016-12-07 16:30:30 -0600

    resolve conflict

--- app/assets/javascripts/register_pop_up.js
+++ app/assets/javascripts/register_pop_up.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 	if ($(window).width() <= 768){
 		var visited = localStorage.getItem('visited');
-		if (visited) {
+		if (!visited) {
 	  		localStorage.setItem('visited', true);
 			$('#signUpModal').modal('show');
 		}
